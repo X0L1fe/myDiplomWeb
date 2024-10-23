7b67cc15c5dbc60881ca7f4a3ef81af6eb128f19
Roman <lighttolight228@gmail.com>
2024-10-23 17:08:22 +0300

    modified:   user_profile/views.py

--- user_profile/views.py
+++ user_profile/views.py
@@ -8,3 +8,16 @@ def profile_view(request):
     messages.success(request, f'Добро пожаловать, {request.user.login}!')
 
     return render(request, 'profile.html')
+
+@login_required
+def profile_update(request):
+    if request.method == 'POST':
+        user = request.user
+        user.passport = request.POST['passport']
+        user.first_name = request.POST['firs_name']
+        user.last_name = request.POST['last_name']
+        user.middle_name = request.POST['middle_name']
+        user.phone = request.POST['phone']
+        user.save()
+        return redirect('profile')  # Перенаправляем обратно на страницу профиля
+    return render(request, 'profile.html')
